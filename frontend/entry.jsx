0146883642216690c909ae204f754bf4f423e3e5
Oleksandr <odiuriagin@gmail.com>
2018-11-13 14:27:08 -0500

    Finish backend user auth

---
+++ frontend/entry.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {login, signup} from './util/session_api_util'
+
+document.addEventListener('DOMContentLoaded', () => {
+  const root = document.getElementById('root');
+  window.login = login;
+  window.signup = signup;
+  ReactDOM.render(<h1>Welcome to RealBnB</h1>, root);
+});
