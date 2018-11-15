01a84ebb989e44ba3a9847e85ad1ecec42982c52
Oleksandr <odiuriagin@gmail.com>
2018-11-15 17:42:05 -0500

    Another commit

--- frontend/components/index/index_properties/index_properties_item.jsx
+++ frontend/components/index/index_properties/index_properties_item.jsx
@@ -7,7 +7,7 @@ const IndexPropertiesItem = ({ property }) => {
     <li>
       <NavLink className="property-link" to={`api/properties/${property.id}`}>
         <img src={"https://goo.gl/T48KR9"} />
-        <p class="property-location">{property.city}, {property.state}</p>
+        <p className="property-location">{property.city}, {property.state}</p>
         <h3 className="property-description">{property.description}</h3>
         <p className="property-price">${property.price}, per night &middot; Free cancellation</p>
       </NavLink>
