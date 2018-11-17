e65505dc21d351830f791f1d2f289c03cf8aa780
Oleksandr <odiuriagin@gmail.com>
2018-11-16 23:33:17 -0500

    Add date picker

--- frontend/components/show/book_form.jsx
+++ frontend/components/show/book_form.jsx
@@ -1,15 +1,39 @@
 import React from 'react';
+import 'react-dates/initialize';
+import { DateRangePicker } from 'react-dates';
+import momentPropTypes from 'react-moment-proptypes';
+import moment from 'moment';
+import PropTypes from 'prop-types';
+import 'react-dates/lib/css/_datepicker.css';
 
-const BookForm = (props) => {
+
+class BookForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      startDate: null,
+      endDate: null,
+      focusedInput: null,
+    }
+  }
+render() {
+
   return (
-    <div>
-      <p className="book-form-price">${props.props.price}<span>   per night</span></p>
+      <div>
       <form>
-        <input type="date"></input>
-        <input type="select"></input>
+        <DateRangePicker
+          startDate={this.state.startDate} // momentPropTypes.momentObj or null,
+          startDateId="start-date" // PropTypes.string.isRequired,
+          endDate={this.state.endDate} // momentPropTypes.momentObj or null,
+          endDateId="end-date" // PropTypes.string.isRequired,
+          onDatesChange={({ startDate, endDate }) => this.setState({ startDate, endDate })} // PropTypes.func.isRequired,
+          focusedInput={this.state.focusedInput} // PropTypes.oneOf([START_DATE, END_DATE]) or null,
+          onFocusChange={focusedInput => this.setState({ focusedInput })} // PropTypes.func.isRequired,
+        />
       </form>
     </div>
   );
+}
 };
 
 export default BookForm;
