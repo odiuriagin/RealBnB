db5bc474bfaf161dac7401d728b6ee9ab97a9f52
Oleksandr <odiuriagin@gmail.com>
2018-12-24 14:30:57 -0500

    Add searchbar to splash page

--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -5,6 +5,12 @@ const Splash = () => {
   return (
     <div className="splash-main">
       <SplashNav />
+      <div className="main-content">
+        <p className="next-trip">Plan your next trip</p>
+        <div className="main-search-container">
+          <input id="main-search" type="text" placeholder='Try "New York"'></input>
+        </div>
+      </div>
     </div>
   )
 }
